2cc8ef1fa46b5df130b8f6834d412e276d546320
bomute kim <kimbomte1996@icloud.com>
2022-08-17 00:32:01 +0900

    お気に入り機能とお気に入りの一覧ページを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :favorites, only: [:index,:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show, :edit, :update] do
-    resources :favorite
+  resources :users, only: [:new, :create, :show, :edit, :update]
   resources :posts do
     collection do
       post :confirm
     end
   end
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-end
+  end

ddaddb8037d132046fbc43837c28aae3b192a808
bomute kim <kimbomte1996@icloud.com>
2022-08-17 17:11:15 +0900

    投稿一覧のページにルーティングのページを設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
     end
   end
 
+  Rails.application.routes.draw do
+    root to: 'posts#index'
+  end
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   end
